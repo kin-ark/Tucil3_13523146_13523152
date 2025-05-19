6b526e82227614db575072177182841e651b5958
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-05-20 02:04:01 +0700

    refactor: remove unused plugin management section

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,16 +5,9 @@
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.8/userguide/multi_project_builds.html in the Gradle documentation.
  */
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    plugins {
-        // Apply the foojay-resolver plugin to allow automatic download of JDKs
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-        id("org.openjfx.javafxplugin") version "0.1.0"
-    }
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "Tucil3_13523146_13523152"
